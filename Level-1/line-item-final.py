77a9ec0205804ca9d3d135c20762ac99fcb7e62d
Pratham Agarwalla <ps2285@srmist.edu.in>
2024-10-12 10:59:41 +0530

    Updates

--- Level-1/line-item-final.py
+++ Level-1/line-item-final.py
@@ -16,7 +16,7 @@ FR_KEY = os.getenv('AZURE_KEY')
 
 # Initialize the Document Analysis Client
 document_analysis_client = DocumentAnalysisClient(
-    endpoint=FR_ENDPOINT, credential=AzureKeyCredential(FR_KEY)
+    endpoint=str(FR_ENDPOINT), credential=AzureKeyCredential(str(FR_KEY))
 )
 
 def extract_invoice_line_items(document, file_name, selected_fields):
