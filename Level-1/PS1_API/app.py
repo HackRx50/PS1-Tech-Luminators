a4124b1a8ba1c02ca710446365a06b3456f8546c
Sachin Kumar <sm8161@srmist.edu.in>
2024-10-12 13:29:16 +0530

    dependencies

--- Level-1/PS1_API/app.py
+++ Level-1/PS1_API/app.py
@@ -1,1 +1,11 @@
+#dependencies
 from flask import Flask, request, jsonify
+import os
+import pandas as pd
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+import io
+
+app = Flask(__name__)
+
