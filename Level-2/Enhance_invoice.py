635aa815cdfce8c346bf63f2f95e1942b5ad4af7
KUNWARRANJEET04 <103445107+KUNWARRANJEET04@users.noreply.github.com>
2024-10-12 18:47:41 +0530

    Text to PDF

--- Level-2/Enhance_invoice.py
+++ Level-2/Enhance_invoice.py
@@ -30,3 +30,10 @@ FR_KEY = os.getenv('AZURE_KEY')
 document_analysis_client = DocumentAnalysisClient(
     endpoint=str(FR_ENDPOINT), credential=AzureKeyCredential(str(FR_KEY))
 )
+
+def extract_text_from_pdf(pdf_file):
+    doc = fitz.open(stream=pdf_file, filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
